773112e2684ca69feed388e77f8e14c8a6fe93ce
Cristeflucas <cristeflucas@hotmail.com>
2026-02-24 20:54:39 -0300

    PracticingC# day2

--- practicingC#/Program.cs
+++ practicingC#/Program.cs
@@ -109,3 +109,176 @@ else
     Console.WriteLine("O aluno recebeu a nota D.");
 }
 
+int valorVenda, soma = 0;
+do
+{
+    Console.Write("Digite o valor da venda (ou 0 para encerrar): ");
+    valorVenda = int.Parse(Console.ReadLine());
+    soma += valorVenda;
+}
+while (valorVenda > 0 && valorVenda != 0);
+Console.WriteLine("Total de vendas do dia: R$" + soma);
+
+
+int quantidade, estoque = 0;
+Console.WriteLine("Deseja adicionar um produto ao estoque?");
+Console.WriteLine("1 - Sim | 0 - Não");
+int resposta = int.Parse(Console.ReadLine());
+while (resposta != 0)
+{
+    Console.WriteLine("Quantidade:");
+    quantidade = int.Parse(Console.ReadLine());
+    estoque += quantidade;
+    Console.Write("Estoque atual: " + estoque);
+    Console.WriteLine();
+    Console.WriteLine("Deseja continuar? ");
+    Console.WriteLine("1 - Sim | 0 - Não");
+    resposta = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Obrigado por usar nosso sistema de estoque!");
+
+
+List<string> alunos = new List<string> { "Ana", "Carlos", "Bianca", "João", "Marina" };
+
+Console.WriteLine("Digite o nome do aluno:");
+string nomeBusca = Console.ReadLine();
+int indice = 0;
+bool encontrado = false;
+
+while (indice < alunos.Count)
+{
+    if (alunos[indice] == nomeBusca)
+    {
+        encontrado = true;
+        break;
+    }
+    indice++;
+}
+
+if (encontrado)
+{
+    Console.WriteLine($"Aluno encontrado na posição: {indice}");
+}
+else
+{
+    Console.WriteLine("Aluno não está presente na lista");
+}
+
+int senhaGerada = 0;
+int senhaAtual = 0;
+int opcao;
+do
+{
+    Console.WriteLine("1 - Gerar nova senha");
+    Console.WriteLine("2 - Chamar próxima senha ");
+    Console.WriteLine("3 - Sair");
+    Console.Write("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            senhaGerada++;
+            Console.WriteLine("Senha gerada: " + senhaGerada.ToString("D3"));
+            break;
+
+        case 2:
+            senhaAtual++;
+            Console.WriteLine("Senha chamada: " + senhaAtual.ToString("D3"));
+            break;
+        case 3:
+            Console.WriteLine("Encerrando o programa. Obrigado!");
+            break;
+    }
+} while(opcao != 3);
+
+int opcao;
+do
+{
+    Console.WriteLine("1 - Celsius para Fahrenheit");
+    Console.WriteLine("2 - Fahrenheit para Celsius");
+    Console.WriteLine("3 - Sair");
+    Console.Write("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+ 
+    switch (opcao)
+    {
+        case 1:
+            Console.Write("Digite a temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius}°C equivalem a {fahrenheit}°F");
+            break;
+        case 2:
+            Console.Write("Digite a temperatura em Fahrenheit: ");
+            double fahr = double.Parse(Console.ReadLine());
+            double cel = (fahr - 32) * 5 / 9;
+            Console.WriteLine($"{fahr}°F equivalem a {cel}°C");
+            break;
+        case 3:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+} while (opcao != 3);
+
+
+for (int i = 1; i <= 20; i++)
+{
+    if (i % 3 == 0)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+
+for (int i = 1; i <= 10; i++)
+{
+    int multiplo = 7 * i;
+    Console.WriteLine($"7 x {i} = {multiplo}" );
+}
+
+
+List<int> notas = new List<int> { 4, 7, 5, 9, 6 };
+
+for (int i = 0; i < notas.Count; i++)
+{
+    if (notas[i] < 6)
+    {
+        Console.WriteLine($"Nota {notas[i]} - Reprovado");
+    }
+    else
+    {
+        Console.WriteLine($"Nota {notas[i]} - Aprovado");
+    }
+}
+
+List<double> notas = new List<double>
+{
+    8.5,
+    6.2,
+    9.1,
+    5.8,
+    7.4
+};
+ 
+double mediaCorte = 7.0;
+ 
+foreach (var nota in notas)
+{
+    if (nota < mediaCorte)
+    {
+        Console.WriteLine($"O aluno com a nota {nota} está abaixo da média!");
+    }
+ 
+    else
+    {
+        Console.WriteLine($"O aluno com a nota {nota} está indo muito bem!");
+    }
+}
+
+
+
